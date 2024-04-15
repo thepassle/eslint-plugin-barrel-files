92a504a557e4279fef5545c06d49adcf643a2a8f
Daniel Holmes <daniel@danielholmes.org>
2024-04-15 13:25:34 +1000

    fix: Consider typescript definitions when detecting barrel files

---
+++ tests/lib/rules/avoid-barrel-files-ts.js
@@ -0,0 +1,76 @@
+/**
+ * @fileoverview avoid-barrel-files-ts
+ * @author Pascal Schilp
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const { RuleTester } = require('@typescript-eslint/rule-tester');
+const { after } = require('node:test');
+const rule = require('../../../lib/rules/avoid-barrel-files.js');
+
+RuleTester.afterAll = after;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+});
+ruleTester.run('avoid-barrel-files ts', rule, {
+  valid: [
+    {
+      code: `
+        type Money = string;
+        export type { Money };
+      `,
+    },
+    {
+      code: `
+        type Money = {
+          amount: string;
+          currency: string;
+        };
+        export type { Money };
+      `,
+    },
+    {
+      code: `
+        interface Money {
+          amount: string;
+          currency: string;
+        };
+        type Country = string;
+        type State = {
+          name: string;
+        };
+        const newSouthWales = {
+          name: "New South Wales"
+        };
+        export { newSouthWales }
+        export type { Money, Country, State };
+      `,
+    }
+  ],
+
+  invalid: [
+    {
+      code: `
+        import { Country } from 'geo';
+        type Money = string;
+        type State = {
+          name: string;
+        };
+        interface Person { name: string; age: number; }
+        export type { Money, Country, Person, State };
+      `,
+      errors: [{ message: 'Avoid barrel files, they slow down performance, and cause large module graphs with modules that go unused.' }],
+    }
+  ],
+});
